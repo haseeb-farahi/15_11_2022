51b528ff5aeba005ddf10ab850f7a385ba3249ae
haseeb-farahi <111962155+haseeb-farahi@users.noreply.github.com>
2022-11-15 12:38:20 +0430

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import './App.css';
+// import MuiGallery from './components/MuiGallery';
+// import MuiSelect from './components/MuiSelect';
+// import MuiButtons from "./components/MuiButtons"
+// import MuiTypography from './components/MuiTypography';
+// import MuiTextField from './components/MuiTextField';
+// import MuiAutocom from './components/MuiAutocom';
+// import MuiSwitchBtn from './components/MuiSwitchBtn';
+// import MuiCard from './components/MuiCard';
+// import MuiBox from './components/MuiBox';
+import MuiGraid from './components/MuiGraid';
+import MuiGallery from './components/MuiGallery';
+import MuiAccordin from './components/MuiAccordion';
+import MuiAlert from './components/MuiAlert';
+import MuiBadge from './components/MuiBadge';
+import MuiChip from './components/MuiChip';
+import MuiToNavBar from './components/MuiToNavBar';
+
+
+function App(){
+              return(
+                <div className='App'>
+                    {/* {/* <MuiButtons /> */}
+                     {/* <MuiTypography/> 
+                    <MuiTextField/>
+                    <MuiSelect/>
+                    <MuiAutocom/>
+                    <MuiSwitchBtn/> */}
+                    {/* <MuiCard/> */}
+                    {/* <MuiBox/> */}
+                    {/* <MuiGraid/> */}
+                    <MuiGallery/>
+                    {/* <MuiAccordin/> */}
+                    <MuiAlert/>
+                    <MuiBadge/>
+                    <MuiChip/>
+                    <MuiToNavBar/>
+                </div>
+                );
+
+      }
+export default App;
